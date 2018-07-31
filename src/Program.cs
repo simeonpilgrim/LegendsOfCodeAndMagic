fc256fb356e792595763eac413701c96b2777e31
Simeon Pilgrim <simeon.pilgrim@gmail.com>
2018-07-31 16:34:33 +1200

    Add C# project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LegendsOfCodeAndMagic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
